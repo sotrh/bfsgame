c28df61933ec8ce13ecf3e735a6df528224e1872
sotrh <bhbenjaminhansen@gmail.com>
2017-05-10 23:26:16 -0600

    grid is now loaded from a file and rendered

--- core/src/io/sotrh/games/bfs/BFSGame.kt
+++ core/src/io/sotrh/games/bfs/BFSGame.kt
@@ -2,16 +2,26 @@ package io.sotrh.games.bfs
 
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import io.sotrh.games.bfs.grid.Grid
+import io.sotrh.games.bfs.grid.gridFromPixmap
 
 class BFSGame : ApplicationAdapter() {
 
     private lateinit var texture: Texture
     private lateinit var batch: SpriteBatch
+    private lateinit var grid: Grid
+    private var offsetX = 0f
+    private var offsetY = 0f
+
+    companion object {
+        private var SCALE = 50f
+    }
 
     override fun create() {
         Pixmap(1, 1, Pixmap.Format.RGB888).using {
@@ -20,13 +30,37 @@ class BFSGame : ApplicationAdapter() {
             texture = Texture(it)
         }
 
+        Pixmap(Gdx.files.internal("maze-1.png")).using {
+            offsetX = (Gdx.graphics.width - it.width * SCALE) * 0.5f
+            offsetY = (Gdx.graphics.height - it.height * SCALE) * 0.5f
+            grid = gridFromPixmap(it)
+        }
+
         batch = SpriteBatch()
     }
 
     override fun render() {
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit()
+            return
+        }
+
+        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1.0f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
-        batch.batched {
-            it.draw(texture, 10f, 10f, 100f, 100f)
+
+        batch.batched { batch ->
+            grid.forEachIndexed { x, list ->
+                list.forEachIndexed { y, cell ->
+                    batch.color = Color(cell)
+                    batch.draw(
+                            texture,
+                            x * SCALE + offsetX,
+                            y * SCALE + offsetY,
+                            SCALE,
+                            SCALE
+                    )
+                }
+            }
         }
     }
 
